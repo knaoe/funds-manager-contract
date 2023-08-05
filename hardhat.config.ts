73180056887bcc859715a40426a942cac2ceafcd
Kenichi Naoe <ken@naoe.tokyo>
2023-08-05 17:49:43 +0900

    :green_heart: install gas-reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,24 +2,37 @@ import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-gas-reporter";
 
 dotenvConfig({ path: __dirname + "/.env" });
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
-const MNEMONIC =
-  process.env.MNEMONIC ||
-  "test test test test test test test test test test test junk";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.21",
+  solidity: {
+    version: "0.8.21",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     hardhat: {},
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
+      accounts: [
+        DEPLOYER_PRIVATE_KEY ?? "0x1234567890123456789012345678901234567890",
+      ],
     },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
 
