e67b42759540769c9f544237382c645886bbd98a
Kenichi Naoe <ken@naoe.tokyo>
2023-08-05 17:04:38 +0900

    :sparkles: add mainnet as network in hardhat config.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,26 @@
+import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
+dotenvConfig({ path: __dirname + "/.env" });
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const MNEMONIC =
+  process.env.MNEMONIC ||
+  "test test test test test test test test test test test junk";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.21",
+  networks: {
+    hardhat: {},
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -34,7 +34,9 @@ main().catch((error: Error) => {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
